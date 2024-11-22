7087bfd5f2878605dd2b015eeb0d160e380b23d0
okbug <1583978087@qq.com>
2024-11-23 01:05:24 +0800

    feat: remove

--- rust_playground/target/debug/deps/rust_playground-a7ebd8daf4f18d82.d
+++
@@ -1,5 +0,0 @@
-/Users/ajunge/code/new_learn/rust_playground/target/debug/deps/rust_playground-a7ebd8daf4f18d82: src/main.rs
-
-/Users/ajunge/code/new_learn/rust_playground/target/debug/deps/rust_playground-a7ebd8daf4f18d82.d: src/main.rs
-
-src/main.rs:
--- rust_playground/target/debug/rust_playground.d
+++
@@ -1,1 +0,0 @@
-/Users/ajunge/code/new_learn/rust_playground/target/debug/rust_playground: /Users/ajunge/code/new_learn/rust_playground/src/main.rs
